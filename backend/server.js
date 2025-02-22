9c2b3f0e0649bf722c55682164debe9f6aded143
Manu <emmanuelkyalo91@gmail.com>
2025-02-22 18:04:00 +0300

    backend commit

--- backend/server.js
+++ backend/server.js
@@ -5,30 +5,31 @@ dotenv.config();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import authRotes from "./src/routes/authRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
 import { connectDB } from "./src/config/db.js";
-
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-app.use(cors());
-
-//MIDDLEWARE
-
+// MIDDLEWARE
 app.use(express.json());
 app.use(cookieParser());
-//ROUTES
+
+// ROUTES
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.use("/api/auth", authRotes);
+app.use("/api/auth", authRoutes);
 
-
-//START SERVER
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
+// START SERVER
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
-  connectDB();
+  await connectDB();
 });
